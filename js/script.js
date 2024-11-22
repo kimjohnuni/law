e36fa2fa56bcbd03054173169ce5e6e2eb72f885
kimjohnuni <166816186+kimjohnuni@users.noreply.github.com>
2024-11-22 18:13:37 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -203,135 +203,143 @@ items.forEach(item => {
       switch(partnerId) {
           case "1":
             content = `
-            <h2>주대진</h2>
+            <h2>옥준원</h2>
             <div class="position-title">대표변호사</div>
-            <p>주대진 변호사(사법시험 28회, 연수원 18기)는 오랜 변호사 경력을 바탕으로 법무법인(유한) 에스제이파트너스에서 다루는 각종 사건에 대한 조언과 지원을 아끼지 않고 있습니다.</p>
+            <p>옥준원 변호사는 사법연수원 수료(15기) 후, 전국 각 지방 및 고등검찰청에서 검사 및 부장검사로 만 23년간 근무 후 퇴직하였고, 이후 변호사로 개업하여 법무법인(유한) 에이스에서 만 12년간 변호사로 재직하였습니다. 검찰에 재직하는 동안 특수부와 공안부에도 근무하였으나 주로 형사부와 조사부에 근무하면서 다양한 형사사건을 수사하였고, 조직범죄, 기업형경제범죄, 다단계사기범죄, 탈세범죄 등 복잡다단한 고소사건을 수사하였습니다.</p>
             `;
               break;
           case "2":
           content = `
-          <h2>Partner Name</h2>
-          <div class="position-title">Position Title</div>
-          <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+          <h2>김정학</h2>
+          <div class="position-title">대표변호사</div>
+          <p>김정학 변호사는 부산 경남중·고(25회)와 서울법대(29회)를 졸업한 후 사법연수원(18기)을 수료하고, 인천지법, 창원지법 통영지원, 서울 남부, 서부, 북부, 중앙, 가정법원 판사와 서울고등법원 판사를 거쳐 부산지법, 수원지법, 서울 서부지법, 중앙지법, 인천지법 부장판사로 약30년간 근무한 후 2018. 1. 퇴직하였습니다. 좀더 구체적 학력, 경력, 성품 등은 네이버 블로그명 ‘김정학변호사’에서 충분히 확인하실 수 있습니다. 오랜 기간 민 ·형사, 행정, 가사 등 다방면에 걸친 재판 경력으로 폭 넓고 질 높은 법률 소양과 경험을 갖추고 있고 각종 소송 현장에서 재판을 한 만큼 남다른 실무경험과 실효적인 법률지식을 바탕으로 믿을 수 있고 성공적인 소송수행과 법률자문을 할 수 있습니다.
+          </p>
           `;
             break;
           case "3":
           content = `
-          <h2>Partner Name</h2>
-          <div class="position-title">Position Title</div>
-          <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+          <h2>주대진</h2>
+          <div class="position-title">대표변호사</div>
+          <p>주대진 변호사(사법시험 28회, 연수원 18기)는 오랜 변호사 경력을 바탕으로 법무법인(유한) 에스제이파트너스에서 다루는 각종 사건에 대한 조언과 지원을 아끼지 않고 있습니다.</p>
           `;
             break;
           case "4":
           content = `
-          <h2>Partner Name</h2>
-          <div class="position-title">Position Title</div>
-          <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+          <h2>송범준</h2>
+          <div class="position-title">구성원변호사</div>
+          <p>송범준 변호사의 주요 업무는 해외투자 및 기업인수합병(M&A) 관련 자문, 특허 침해, IP 분쟁, 가상화폐 관련 민형사 자문 및 송무입니다.<br><br>관련 분야: 해외투자, 인수합병, 기업법무, 수사대응, 가상화폐, 지적재산권(IP 및 상표권) 관련 민형사 자문 및 송무
+          </p>
           `;
             break;
           case "5":
           content = `
-          <h2>Partner Name</h2>
-          <div class="position-title">Position Title</div>
-          <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+          <h2>장현경</h2>
+          <div class="position-title">구성원변호사</div>
+          <p>장현경 변호사는 대한변호사협회에 형사법과 손해배상 전문 변호사로 등록되어 있으면서, 검찰총장 출신 등이 포진하고 있는 법무법인 대건에서 파트너 변호사로 근무하며 다수의 형사사건과 민사 사건을 성공적으로 처리한 바 있습니다. 또한, 대기업 대금청구 사건 및 하도급법 등과 관련된 다수의 공정거래위원회 사건을 담당한 바 있으며, 2014년 중앙선거관리위원회에서 진행한 선거용어사전 편찬에 참여하고 2018년 서울에서 열린 세계헌법대회 조직위원회에도 활동하였을 정도로 공법 분야에서도 그 역량을 보이고 있을 뿐만 아니라, 현재 투자회사의 준법감시인 역할을 겸하면서 그 전문성을 광범위한 분야로 넓히고 있습니다.</p>
           `;
             break;
           case "6":
           content = `
-          <h2>Partner Name</h2>
-          <div class="position-title">Position Title</div>
-          <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+          <h2>이석재</h2>
+          <div class="position-title">구성원변호사</div>
+          <p>이석재 변호사는 1997년 국립세무대학을 졸업한 후, 서초·용산·종로세무서에서 법인 및 개인세금 관련 업무를, 2001년부터는 서울지방국세청 조사1국에서 세무조사업무에 종사하였으며, 특히 조사4국에서 특별세무조사와 주식변동조사에 관한 풍부한 실무경험을 쌓았습니다.<br><br>국세청 퇴직 후에는 9급 신화 박찬욱 서울지방국세청장과 PB세무컨설팅을 창업하여 세무사로서 세금신고 및 세무회계조정, 세무조사대응, 불복청구, 유권해석 및 조세전략 등 제반 조세문제를 균형잡힌 시각에서 다뤄왔습니다.<br><br>2019년부터 법무법인 호연에서 변호사, 법무법인(유한) 평산에서 파트너 변호사로서 조세형사, 행정소송 및 법률사무에까지 그 업무영역을 넓혔습니다.<br><br>2024년 법무법인(유한) 에스제이파트너스에 합류한 이석재 구성원 변호사는 그동안 조세전반의 다양한 분야에서 쌓아온 실무경험을 살려 고객에게 최고의 변호사가 될 것을 약속합니다.
+          </p>
           `;
             break;
           case "7":
           content = `
-          <h2>Partner Name</h2>
-          <div class="position-title">Position Title</div>
-          <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+          <h2>옥민석</h2>
+          <div class="position-title">구성원변호사</div>
+          <p>옥민석 변호사는 제1보병사단 헌병대 수사과, 성범죄 전문 로펌에서 근무하였습니다.<br><br>옥민석 변호사는 다수의 성범죄, 음주운전 사건을 해결하며 얻은 경험과 노하우를 바탕으로 형사법 분야에서 최상의 법률서비스를 제공하고 있습니다.<br><br>옥민석 변호사는 ‘N번방 사건’, ‘박사방 사건’ 등과 같은 아동·청소년의성보호에관한법률위반 사건, ‘윤드로저 사건’ 등과 같은 성폭력범죄의처벌등에관한특례법위반 사건, 강제추행, 강간 등 사건을 연이어 성공적으로 해결하며 성범죄 분야에서 그 전문성을 인정받고 있습니다.<br><br>옥민석 변호사는 ‘윤창호 사건’ 이후 음주운전에 대한 처벌이 강화된 상황에서 벌금형 등 선처를 이끌어내며 음주운전 사건에서도 그 두각을 나타내고 있습니다.<br><br>옥민석 변호사는 소비자들의 선택을 받아 ‘2021 KCA 우수 전문인 어워즈’를 수상하였습니다.
+          </p>
           `;
             break;
           case "8":
           content = `
-          <h2>Partner Name</h2>
-          <div class="position-title">Position Title</div>
-          <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+          <h2>이창우</h2>
+          <div class="position-title">구성원변호사</div>
+          <p>이창우 변호사는 법무법인 중원에서 법원장, 부장판사, 검사 출신의 변호사들과 함께 민사, 형사, 행정, 집행 등 다양한 유형의 소송과 상장기업, 지방자치단체, 사립학교법인, 교육청, 국세청 등에 대한 자문 업무를 수행하면서 얻은 전문 지식을 바탕으로 의뢰인들의 권익을 보호하기 위하여 최적화된 법률서비스를 제공하고 있습니다.</p>
           `;
             break;
             case "9":
             content = `
-            <h2>Partner Name</h2>
-            <div class="position-title">Position Title</div>
-            <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+            <h2>강지웅</h2>
+            <div class="position-title">구성원변호사</div>
+            <p>강지웅 변호사의 주된 업무 분야는 명예훼손, 모욕 등의 사회범죄에 관한 형사사건 및 이혼(재산분할, 상간자 소송), 상속(상속재산분할, 후견인 지정) 등의 가사사건 업무입니다. 이 외에도, 다수의 부동산 분쟁과 손해배상 소송 등의 민사사건을 두루 수행하였고, 마약, 성범죄, 사기, 횡령 등의 형사사건을 통해 의뢰인의 권익보호에 앞장서 왔으며, 음주운전과 관련한 형사 및 관련 행정소송에서 뚜렷한 성과를 보여왔습니다.</p>
             `;
               break;
               case "10":
               content = `
-              <h2>Partner Name</h2>
-              <div class="position-title">Position Title</div>
-              <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+              <h2>이동현</h2>
+              <div class="position-title">구성원변호사</div>
+              <p>대한변호사협회학교폭력전문변호사, 대한변호사협회형사전문변호사<br><br>강릉고등학교, 법과대학법학과(수석졸업), ROTC 보병장교전역, 법학전문대학원(차석입학), 중국소주대학교왕건법학원교환학생, 변호사시험합격, 법무법인(유한) 태평양인턴<br><br>前) 신영증권사내변호사 前) 서울자양초등학교학교폭력대책위원회위원 前) 서울중마초등학교학교폭력대책위원회위원 前) 서울성동광진교육지원청학교폭력대책심의위원회위원 前) 김포교육지원청학교폭력대책심의위원회위원<br><br>現) 서울시교육청행정심판위원회위원 現) 서울특별시교육청성희롱·성폭력사안처리지원단자문·심의위원 現) 광주하남교육지원청학교폭력대책심의위원회위원장 現) 안양과천교육지원청학교폭력대책심의위원회위원 現) 군포의왕교육지원청학교폭력대책심의위원회위원 現) 안양만안경찰서여성청소년과선도심사위원회전문위원 現) 서울자양초등학교교권보호위원회외부위원 現) 서울자양초등학교성희롱•성폭력외부전문위원 現) 서울자양초등학교변호사명예교사 現) 서울매헌초등학교변호사명예교사 現) 서울지방변호사회미래인재지원단위원 現) 서울지방변호사회미래인재특별위원회위원 現) 서울지방변호사회법제사법위원회위원 現) 대한변호사협회입법특별위원회위원 現) 강남경찰서경찰발전협의회위원 現) 관악경찰서경찰발전협의회회원 現) 한국법학원대의원 現) 서울금융고등학교성고충심의위원회위원 現) 합격의법학원형사법강사(변호사시험강의) 現) 동국대학교사범대학부속여자고등학교고문변호사 現) 길음중학교고문변호사 現) 문정중학교고문변호사 現) 방학중학교고문변호사 現) 명일중학교고문변호사 現) 성일중학교고문변호사 現) 신남중학교고문변호사 現) 동국대학교사범대학부속여자중학교고문변호사 現) 신원초등학교고문변호사 現) 장안초등학교고문변호사 現) 매헌초등학교고문변호사
+              </p>
               `;
                 break;
                 case "11":
                 content = `
-                <h2>Partner Name</h2>
-                <div class="position-title">Position Title</div>
-                <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+                <h2>하서정</h2>
+                <div class="position-title">변호사</div>
+                <p>[학력] 서울대학교 경영학과<br><br>[경력] (전) 서울지방변호사회 회원이사 (현) 서울지방변호사회 교육이사 (현) 여성변호사회 문화이사 (현) 대한변호사협회 대의원 (현) 서울중앙지방법원 조기조정위원 (현) 노원경찰서 징계심의위원회 위원 (현) 인천공항본부세관 법률고문 (현) 서울시물재생위원회 위원 (전) 광진경찰서 정보공개심의위원회 위원 (현) 서초구청 정보공개심의위원회 위원 (현) 남양주 사회적경제육성위원회 위원 (현) 서울경제진흥원 심사위원회 위원 (현) 서울시 사이버성폭력센터 법률전담위원 (전) 강동송파교육지원청 학교폭력심의위원회 위원 (전) 남부교육지원청 학교폭력심의위원회 위원 (현) 수서경찰서 정보공개심의위원회 위원 (현) 서울경제진흥원 규제개혁지원단 전문위원 (현) 대한변호사협회 광고심사위원회 위원 (현) 서울지방변호사회 미디어특별위원회 위원 (현) 중소벤처기업법포럼 상임이사 (전) 중소벤처기업부 법률전문위원 (현) 대한변호사협회 이민출입국변호사회 이사 (현) 대한변호사협회 등기경매변호사회 정회원 / 2021 가사연수원 수료 / 2019 조세연수원 수료
+                </p>
                 `;
                   break;
                   case "12":
                   content = `
-                  <h2>Partner Name</h2>
-                  <div class="position-title">Position Title</div>
-                  <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+                  <h2>김석훈</h2>
+                  <div class="position-title">변호사</div>
+                  <p>김석훈 변호사는 연세대학교 경영학과와 동 대학교 법학과 일반대학원(석사, 민법전공)을 졸업하였고, 헌법재판소에서 실무수습을 한 경력이 있습니다.<br><br>현재 에스제이파트너스에서 행정심판, 행정소송 등 행정사건과 이혼, 상속 등 가사사건과 일반 민·형사 사건을 담당하고 있습니다.
+</p>
                   `;
                     break;
                     case "13":
                     content = `
-                    <h2>Partner Name</h2>
-                    <div class="position-title">Position Title</div>
-                    <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+                    <h2>박재성</h2>
+                    <div class="position-title">변호사</div>
+                    <p>박재성 변호사는 서울대학교 법학부를 졸업하고 변호사시험에 합격한 뒤, 에스제이파트너스에서 민사, 형사, 행정, 노무, 의료분쟁, 병원 및 기업자문 등의 다양한 업무를 수행하고 있습니다.<br><br>박재성 변호사는 현재 서울지방변호사회 형사당직변호사, 대한병원협회 준법지원인, 공군검찰부 국선변호인(피해자) 등으로 활동하고 있으며, 자문 및 송무전문 변호사로서 다수의 업무를 수행하면서 쌓아온 전문성과 사안의 핵심을 정확하게 파악하는 능력을 바탕으로 고객의 모든 법률적 고민에 대하여 명쾌하면서도 최선의 답을 드리고자 노력하는 변호사입니다.</p>
                     `;
                       break;
                       case "14":
                       content = `
-                      <h2>Partner Name</h2>
-                      <div class="position-title">Position Title</div>
-                      <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+                      <h2>이정석</h2>
+                      <div class="position-title">변호사</div>
+                      <p>이정석 변호사는 연세대학교 법과대학을 졸업하였으며, 대한변호사협회 등록 형사법 전문변호사입니다.<br><br>주로 형사사건과 기업자문을 담당하고 있으며, 서울서부지방법원 국선변호인, 서울지방변호사협회 형사당직변호사, 하남경찰서 수사자문변호사로도 활동중입니다.</p>
                       `;
                         break;
                         case "15":
                         content = `
-                        <h2>Partner Name</h2>
-                        <div class="position-title">Position Title</div>
-                        <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+                        <h2>홍후혁</h2>
+                        <div class="position-title">변호사</div>
+                        <p>홍후혁 변호사는 민·형사사건을 수행하고 있고, 특히 강제추행, 카메라등이용촬영, 성착취물소지, 통신매체이용음란 등 성범죄 사건에서 다수의 성공사례를 보유하고 있습니다. 또한 서울지방변호사회 형사당직 변호사로 활동하고 있습니다.</p>
                         `;
                           break;
                           case "16":
                           content = `
-                          <h2>Partner Name</h2>
-                          <div class="position-title">Position Title</div>
-                          <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+                          <h2>홍민</h2>
+                          <div class="position-title">변호사</div>
+                          <p>홍민 변호사는 PF사업 및 기타 각종 부동산개발, 공사대금 및 지체상금, 건축물에 대한 하자, 재건축, 재개발 기타 도시정비사업 등 각종 부동산 관련 소송 및 자문, 임시주주총회허가, 주주총회결의 취소, 직무집행정지가처분, 회계장부열람등사가처분 등 주식회사 관련 소송 및 자문, 보험 관련 소송 및 자문, 가압류, 가처분 등 보전처분 및 청구이의 등 집행 관련 소송 및 자문, 사기, 횡령, 배임, 명예훼손, 성범죄 등 일반 형사사건의 수사 및 재판 대응</p>
                           `;
                             break;
                             case "17":
                             content = `
-                            <h2>Partner Name</h2>
-                            <div class="position-title">Position Title</div>
-                            <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+                            <h2>정지수</h2>
+                            <div class="position-title">변호사</div>
+                            <p>정지수 변호사는 전주 상산고등학교와 고려대학교를 졸업하였고, 부산지방법원과 검찰에서 실무수습을 한 경력이 있습니다. 현재 에스제이파트너스의 소속 변호사로서 가상화폐 관련 민형사 자문 및 송무, 기업법무, 인수합병, 이혼/상속 업무를 수행하고 있습니다.</p>
                             `;
                               break;
                               case "18":
                               content = `
-                              <h2>Partner Name</h2>
-                              <div class="position-title">Position Title</div>
-                              <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+                              <h2>최형기</h2>
+                              <div class="position-title">변호사</div>
+                              <p>[주요업무분야] 중소벤처기업부 출신의 중소기업 및 소상공인 법률 자문 및 관련 송무 수행, 관련 상가 부동산 및 임대차 사건 수행, 관련 행정소송 사건 수행, 기타 민형사 사건 수행<br><br>[경력] 의정부지방법원 인턴, 의정부지방검찰청 인턴, 법무법인 동하, 중소벤처기업부, (현) 법무법인(유한) 에스제이파트너스 소속변호사<br><br>[학력] 의정부고등학교, 성균관대학교 법과대학, 충남대학교 법학전문대학원
+                              </p>
                               `;
                                 break;
                                 case "19":
                                 content = `
-                                <h2>Partner Name</h2>
-                                <div class="position-title">Position Title</div>
-                                <p>Detailed description about the partner. This can be multiple sentences long and will maintain proper spacing and line height for readability.</p>
+                                <h2>남준규</h2>
+                                <div class="position-title">외국변호사(D.C.)</div>
+                                <p>남준규 외국변호사는(미국, 워싱턴 D.C.), 공공기관, 기업 및 로펌 등을 거치면서 기업의 투자, M&A 등과 관련된 업무를 성실하게 수행해 왔습니다. 다양한 국내외 경험을 바탕으로 국내/해외 투자, 국제상거래, M&A, JV, 라이센싱, 국제중재 등과 같이 기업의 거래 및 운영에 관한 자문 업무를 주로 수행하고 있습니다.<br><br>Junkyu Nam (JK) is a foreign attorney (Washington D.C.) in SJ Partners Law Office. His practice focuses on Corporate law, M&A, Domestic and Cross-border Investment, International commerce, JV, licensing. Prior to joining Song Bo, Junkyu (JK) had worked pu</p>
                                 `;
                                   break;
                                   case "20":
@@ -427,6 +435,7 @@ items.forEach(item => {
   });
 });
 });
+
 
 
 
